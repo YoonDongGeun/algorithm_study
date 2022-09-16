c9b375d045a548a359ad6142205a7ddc3a6204f0
GyeongSam <rud7tka@gmail.com>
2022-09-16 17:10:33 +0900

    0916 알고리즘 스터디

--- 송경삼/0914_Cpp_백준_1937_욕심쟁이 판다/0914_Cpp_백준_1937_욕심쟁이 판다.cpp
+++ 송경삼/0914_Cpp_백준_1937_욕심쟁이 판다/0914_Cpp_백준_1937_욕심쟁이 판다.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 struct ls {
 	int I;
 	int J;
@@ -19,48 +18,42 @@ int di[4] = { 1,0,-1,0 }, dj[4] = { 0,1,0,-1 };
 
 bool compare(ls a, ls b) {
 	return (a.C < b.C);
-}
+}																					//sort용 비교함수
 
 int main() {
 	scanf("%d", &N);
 	int a = -1;
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < N; ++j) {
-			scanf("%d", &forest[i][j]);
+			scanf("%d", &forest[i][j]);					//input을 받아서 
 			sub.I = i;
 			sub.J = j;
 			sub.C = forest[i][j];
-			od[++a] = sub;
+			od[++a] = sub;								//od 배열에 담습니다.
 		}
 	}
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < N; ++j) {
-			value[i][j] = 1;
+			value[i][j] = 1;								//value를 초기화 해주고
 		}
 	}
 
+	sort(od, od + N * N, compare);	//대나무 값에 따라 정렬을 합니다
 
-	sort(od, od + N * N, compare);
-	for (int i = 0; i < N*N; ++i) {
-	/*	printf("i:%d j:%d v:%d\n", od[i].I, od[i].J, od[i].C);*/
-	}
-	for (int i = 0; i < N*N; ++i) {
+	for (int i = 0; i < N*N; ++i) {	// 가장 작은값부터
 		sub = od[i];
 		sub3 = 0;
-		for (int d = 0; d < 4; ++d) {
+		for (int d = 0; d < 4; ++d) {	//주변을 둘러보면서
 			sub2.I = sub.I + di[d];
 			sub2.J = sub.J + dj[d];
 			sub2.C = forest[sub2.I][sub2.J];
 			if (sub2.I >= 0 && sub2.I < N && sub2.J >= 0 && sub2.J < N && sub.C>sub2.C) {
 				if (sub3 < value[sub2.I][sub2.J]) sub3 = value[sub2.I][sub2.J];
 			}
-		}
-		value[sub.I][sub.J] = sub3 + 1;
+		}																// 자기보다 작은값들 중에
+		value[sub.I][sub.J] = sub3 + 1; // 제일 큰 값 +1로 갱신해주고
 		if (value[sub.I][sub.J] > MAX) MAX = value[sub.I][sub.J];
-	}
-	printf("%d", MAX);
-
-
-
+	}												
+	printf("%d", MAX);//출력합니다.
 	
 }
