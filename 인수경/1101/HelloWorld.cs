2438492e0cab34930900e907bf682c00f091250b
SugyoungIn <isk2838@gmail.com>
2022-11-02 00:52:17 +0900

    221101 c# hello world!

---
+++ 인수경/1101/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
